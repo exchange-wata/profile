ea706f1e46a8aeeaddf242090288dc59690adefc
exchange-wata <exchange.wata0907@gmail.com>
2018-11-04 14:58:38 +0800

    change favicon

--- my_profile.php
+++ my_profile.php
@@ -77,7 +77,8 @@
 
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"> -->
-	<link rel="icon" href="favicon.ico">
+	<!-- <link rel="icon" href="favicon.ico"> -->
+	<link rel="icon" href="assets/images/favicons_basashi/favicon.ico">
 
 	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
